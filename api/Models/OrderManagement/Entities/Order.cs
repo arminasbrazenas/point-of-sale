737cffe595ae5963b88455ec42dd776e32bf6385
vestacic <vesta.cic@gmail.com>
2024-12-11 16:38:14 +0200

    modify order

--- api/Models/OrderManagement/Entities/Order.cs
+++ api/Models/OrderManagement/Entities/Order.cs
@@ -1,3 +1,5 @@
+using PointOfSale.Models.ApplicationUserManagement.Entities;
+using PointOfSale.Models.BusinessManagement.Entities;
 using PointOfSale.Models.OrderManagement.Enums;
 using PointOfSale.Models.Shared.Entities;
 
@@ -5,6 +7,8 @@ namespace PointOfSale.Models.OrderManagement.Entities;
 
 public class Order : EntityBase<int>
 {
+    public required int BusinessId { get; set; }
+    public Business Business { get; set; } = null!;
     public required OrderStatus Status { get; set; }
     public required List<OrderItem> Items { get; set; }
     public required List<OrderServiceCharge> ServiceCharges { get; set; }
