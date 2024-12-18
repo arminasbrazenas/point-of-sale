a3d52af5e8e9be17dc944b8e1f7ac5ad6fe1f5b7
Arminas Brazenas <arminas.brazenas@gmail.com>
2024-12-18 18:44:44 +0200

    Add missing authorization

--- api/Api/Controllers/ReservationsController.cs
+++ api/Api/Controllers/ReservationsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PointOfSale.BusinessLogic.OrderManagement.DTOs;
 using PointOfSale.BusinessLogic.OrderManagement.Interfaces;
@@ -7,6 +8,7 @@ using PointOfSale.DataAccess.OrderManagement.Filters;
 namespace PointOfSale.Api.Controllers;
 
 [ApiController]
+[Authorize(Roles = "BusinessOwner,Employee")]
 [Route("v1/reservations")]
 public class ReservationsController : ControllerBase
 {
